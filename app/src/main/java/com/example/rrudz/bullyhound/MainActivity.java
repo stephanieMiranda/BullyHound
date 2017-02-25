b2c0c2da6382c0961039db1dfcdffa43e04e83f2
Rafal Rudzinski <rrudzin1@asu.edu>
2017-02-25 14:45:19 -0800

    added twitter API

--- app/src/main/java/com/example/rrudz/bullyhound/MainActivity.java
+++ app/src/main/java/com/example/rrudz/bullyhound/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.rrudz.bullyhound;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "rrudzinskipl";
+    private static final String TWITTER_SECRET = "bialystok821";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
         setContentView(R.layout.activity_main);
     }
+
+
 }
