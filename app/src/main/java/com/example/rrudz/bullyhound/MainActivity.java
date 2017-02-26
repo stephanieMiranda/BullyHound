e6e4d8bfd2435e1d09b223f3ae8dc29f0938c0e8
Rafal Rudzinski <rrudzin1@asu.edu>
2017-02-26 03:01:12 -0800

    hokked mainactivity to httphandler

--- app/src/main/java/com/example/rrudz/bullyhound/MainActivity.java
+++ app/src/main/java/com/example/rrudz/bullyhound/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.rrudz.bullyhound;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import java.util.ArrayList;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = MainActivity.class.getSimpleName();
+    private ListView list;
+
+    ArrayList<Tweet> tweetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.list_item);
+
+        tweetList = new ArrayList<>();
+        list = (ListView) findViewById(R.id.list_view);
+
+        new GetTweets().execute();
+    }
+
+    private class GetTweets extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            Toast.makeText(MainActivity.this, "JSON data is downloading", Toast.LENGTH_LONG).show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+
+            HttpHandler httphandler = new HttpHandler();
+
+            // make request to URL
+            String url = "https://api.twitter.com/1.1/search/tweets.json?q=%40rrudzinskipl&src=typd";
+            String jsonString = httphandler.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonString);
+
+            if (jsonString != null) {
+                JsonParse tweets = new JsonParse(jsonString);
+                tweetList = tweets.getStatuses();
+
+
+
+
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(), "Couldn't get json from server, check logcat",
+                                Toast.LENGTH_LONG).show();;
+                    }
+                });
+            }
+
+            return null;
+        }
     }
 }
--- app/src/main/java/com/example/rrudz/bullyhound/TimelineActivity.java
+++ app/src/main/java/com/example/rrudz/bullyhound/TimelineActivity.java
@@ -1,24 +1,20 @@
 package com.example.rrudz.bullyhound;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
-import com.twitter.sdk.android.tweetui.UserTimeline;
-
-import io.fabric.sdk.android.Fabric;
-
-public class TimelineActivity extends ListActivity {
+public class TimelineActivity extends AppCompatActivity {
 
      // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    /*
      private static final String TWITTER_KEY = "H8KFZggtlb6revjqZ2ADVD1Fo";
      private static final String TWITTER_SECRET = "1Omxt717VmKS3mGExZjFXYjist6iLMl2B8hHZ0wUUBYanDB0AV";
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        /*
         super.onCreate(savedInstanceState);
         TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
         Fabric.with(this, new Twitter(authConfig));
@@ -32,5 +28,9 @@ public class TimelineActivity extends ListActivity {
         .setTimeline(userTimeline)
         .build();
         setListAdapter(adapter);
+        */
+
+
+
     }
 }
