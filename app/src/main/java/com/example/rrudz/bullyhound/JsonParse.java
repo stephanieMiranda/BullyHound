170ccf8ab4754178bcb460e3c9a8260294feda1f
stryakr <rnsteel1@asu.edu>
2017-02-26 01:39:45 -0800

    Test Implementation of JsonParse

--- app/src/main/java/com/example/rrudz/bullyhound/JsonParse.java
+++ app/src/main/java/com/example/rrudz/bullyhound/JsonParse.java
@@ -1,34 +1,41 @@
 package com.example.rrudz.bullyhound;
 import android.util.Log;
 
-import com.google.gson.*;
-import com.google.gson.stream.JsonReader;
+import org.json.*;
+import java.util.ArrayList;
 
 public class JsonParse {
 
-    JsonObject statuses;
+    private JSONArray statusesArray;
 
     public JsonParse(String searchResult) {
-        JsonElement jelement = new JsonParser().parse(searchResult);
-        JsonObject jsonObject = jelement.getAsJsonObject();
-        if (jsonObject.get("errors") != null)
-        {
-            // add toast logic for malformed response/query
-            Log.e("JSONQuery Malformed", "Please check the query string for correct formedness");
+        try {
+            JSONObject jsonObject = new JSONObject(searchResult);
+            if (jsonObject.get("errors") != null) {
+                // add toast logic for malformed response/query
+                Log.e("JSONQuery Malformed", "Please check the query string for correct formedness");
+            }
+            statusesArray = jsonObject.getJSONArray("statuses");
         }
-        JsonObject statuses = jsonObject.getAsJsonObject("statuses");
+        catch (JSONException je){
+            // Toast Logic for failure
+            Log.e("ConstructorFailure","Error in JsonParse");
+        }
     }
 
-    public TweetObject[] getStatuses() {
-
-        for (int i = 0; i < ) {
-            TweetObject tweetObject = new TweetObject(
-                    js.get("text"),
-                    js.get("screen_name"),
-                    js.get("id"));
+    public ArrayList<Tweet> getStatuses() {
+        ArrayList<Tweet> outputList = new ArrayList<>();
+        for(int i = 0; i < statusesArray.length(); i++) {
+            try {
+                JSONObject element = statusesArray.getJSONObject(i);
+                String name = element.getJSONObject("entities").getJSONArray("user_mentions").getJSONObject(1).get("screen_name").toString();
+                String message = element.getJSONObject("text").toString();
+                Tweet newTweet = new Tweet(name, message, null);
+                outputList.add(newTweet);
+            } catch (JSONException je2) {
+                Log.e("ArrayList", "Failure in getStatuses()");
+            }
         }
-
-
+        return outputList;
     }
-
 }
